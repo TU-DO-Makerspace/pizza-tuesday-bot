ea025ab1ffbf28cc27067065b80bbd0410c88697
Maximilian Eckert <eckertm2000@gmail.com>
2022-02-04 15:04:31 +0100

    telegram bot boilerplate

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
+
+const bot = new Telegraf(process.env.TOKEN);
+
+bot.command("start", (ctx) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "hello there! Welcome to my new telegram bot.",
+    {}
+  );
+});
+
+bot.launch();
