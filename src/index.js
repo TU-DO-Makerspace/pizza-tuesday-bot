c1d462da152e0d21905aa13ae2e643944ef25e7a
Maximilian Eckert <eckertm2000@gmail.com>
2022-03-16 15:26:03 +0100

    3: feat: check for collection with corresponding user id in firestore

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
 const { Telegraf } = require("telegraf");
-const { initializeApp } = require("firebase-admin/app");
+const admin = require("firebase-admin");
+require("firebase-admin/firestore");
 
 // --- initialization
 require("dotenv").config();
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 console.log("Successfully connected to Telegram API");
-const app = initializeApp();
+admin.initializeApp();
 console.log("Successfully connected to Firebase");
+const db = admin.firestore();
+console.log("Successfully connected to Firestore");
 
 // --- commands
-bot.command("start", (ctx) => {
-  console.log(ctx.from);
+bot.command("start", async (ctx) => {
+  console.log(await checkAndCreateUser(ctx));
   bot.telegram.sendMessage(
     ctx.chat.id,
-    "hello there! Welcome to my new telegram bot.",
+    "Yo moin, willkommen beim Pizza Tuesday Bot!",
     {}
   );
 });
+
+const checkAndCreateUser = async (ctx) => {
+  const id = ctx.from.id;
+
+  const collection = db.collection(process.env.FIRESTORE_USER_COLLECTION);
+  const doc = collection.doc(id.toString());
+
+  try {
+    const response = await doc.get();
+
+    if (response.exists) {
+      return "User already exists.";
+    } else {
+      return "User does not yet exist.";
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 // --- processing
 bot.launch();
