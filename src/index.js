3ad6851e9f0822853f12a6f1bcc6c1085663de4b
Maximilian Eckert <eckertm2000@gmail.com>
2022-03-23 17:09:16 +0100

    4: refactor: index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
-const { Telegraf, Scenes, session } = require("telegraf");
-const admin = require("firebase-admin");
-const { getFirestore } = require("firebase-admin/firestore");
-require("firebase-admin/firestore");
-// scenes
-const orderWizard = require("./scenes/order-wizard");
-
-// --- initialization
+// --- imports
 require("dotenv").config();
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
-console.log("Successfully connected to Telegram API");
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // replace `\` and `n` character pairs w/ single `\n` character
-  }),
-});
-console.log("Successfully connected to Firebase Admin");
-const db = getFirestore();
-console.log("Successfully connected to Firestore Firestore");
-const stage = new Scenes.Stage([orderWizard]);
-bot.use(session());
-bot.use(stage.middleware());
+const initBot = require("./init-bot");
+// --- helpers
+const checkAndCreateUser = require("./helpers/check-and-create-user");
+// --- initialization
+const bot = initBot();
 
 // --- commands
 bot.command("start", async (ctx) => {
@@ -38,31 +21,6 @@ bot.command("hunger", async (ctx) => {
 });
 
 // --- helper functions
-const checkAndCreateUser = async (ctx) => {
-  const id = ctx.from.id;
-
-  // collection and document references
-  const collection = db.collection(process.env.FIRESTORE_USER_COLLECTION);
-  const doc = collection.doc(id.toString());
-
-  try {
-    const response = await doc.get(); // try reading from database
-
-    // document exists -> return document data
-    if (response.exists) {
-      const data = response.data();
-      return data;
-    }
-    // document does not exist -> create and return data
-    else {
-      delete ctx.from.id; // id is not necessary at this point
-      doc.set(ctx.from);
-      return ctx.from;
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
 
 // --- processing
 bot.launch();
