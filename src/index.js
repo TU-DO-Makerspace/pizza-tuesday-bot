4aa60fcd0839c1986ca0384ddb1143ebffe0cc3e
Maximilian Eckert <eckertm2000@gmail.com>
2022-03-29 13:10:55 +0200

    4 ordering (#13)
    * 4: feat: order wizard
    
    * 4: feat: prices + small refactoring
    
    * 4: refactor: orderWizard
    
    * 4: refactor: index.js
    
    * 4: feat: create order entry in firestore
    
    * 4: fix: error response
    
    * 4: fix: error response

--- src/index.js
+++ src/index.js
@@ -1,58 +1,33 @@
-const { Telegraf } = require("telegraf");
-const admin = require("firebase-admin");
-const { getFirestore } = require("firebase-admin/firestore");
-require("firebase-admin/firestore");
-
-// --- initialization
+// --- imports
 require("dotenv").config();
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
-console.log("Successfully connected to Telegram API");
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // replace `\` and `n` character pairs w/ single `\n` character
-  }),
-});
-console.log("Successfully connected to Firebase Admin");
-const db = getFirestore();
-console.log("Successfully connected to Firestore Firestore");
+const initBot = require("./init-bot");
+// --- helpers
+const checkAndCreateUser = require("./helpers/check-and-create-user");
+const handleError = require("./helpers/errors");
+// --- initialization
+const bot = initBot();
 
 // --- commands
 bot.command("start", async (ctx) => {
-  const user = await checkAndCreateUser(ctx);
-  bot.telegram.sendMessage(
-    ctx.chat.id,
-    `Yo moin, ${user.first_name}! Willkommen beim Pizza Tuesday Bot!`,
-    {}
-  );
+  try {
+    const user = await checkAndCreateUser(ctx);
+    bot.telegram.sendMessage(
+      ctx.chat.id,
+      `Yo moin, ${user.first_name}! Willkommen beim Pizza Tuesday Bot!`,
+      {}
+    );
+  } catch (err) {
+    handleError(err, bot, ctx);
+  }
 });
 
-const checkAndCreateUser = async (ctx) => {
-  const id = ctx.from.id;
-
-  // collection and document references
-  const collection = db.collection(process.env.FIRESTORE_USER_COLLECTION);
-  const doc = collection.doc(id.toString());
-
+bot.command("hunger", async (ctx) => {
   try {
-    const response = await doc.get(); // try reading from database
-
-    // document exists -> return document data
-    if (response.exists) {
-      const data = response.data();
-      return data;
-    }
-    // document does not exist -> create and return data
-    else {
-      delete ctx.from.id; // id is not necessary at this point
-      doc.set(ctx.from);
-      return ctx.from;
-    }
+    await ctx.scene.enter("ORDER_WIZARD_SCENE_ID");
   } catch (err) {
-    console.error(err);
+    handleError(err, bot, ctx);
   }
-};
+});
 
 // --- processing
 bot.launch();
