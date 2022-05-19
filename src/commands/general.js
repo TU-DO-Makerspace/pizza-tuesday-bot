3665cbc03fa9801b38da67b7fd835a2d368b52c5
Maximilian Eckert <eckertm2000@gmail.com>
2022-05-19 14:52:54 +0200

    28: feat: command setup

--- src/commands/general.js
+++ src/commands/general.js
@@ -4,6 +4,7 @@ import { Composer } from "telegraf";
 // --- helpers
 import handleError from "../helpers/errors.js";
 
+// --- introduction, first time starting the bot
 const start = Composer.command("start", async (ctx) => {
   try {
     return await ctx.telegram.sendMessage(
@@ -15,4 +16,40 @@ const start = Composer.command("start", async (ctx) => {
   }
 });
 
-export default Composer.compose([start]);
+// --- overview over possible commands
+const help = Composer.command("help", async (ctx) => {
+  try {
+    return await ctx.telegram.sendMessage(
+      ctx.chat.id,
+      "TODO: Send help to user"
+    );
+  } catch (err) {
+    handleError(err, ctx, "help");
+  }
+});
+
+// --- get status of current order
+const status = Composer.command("status", async (ctx) => {
+  try {
+    return await ctx.telegram.sendMessage(
+      ctx.chat.id,
+      `TODO: Send status updates to user`
+    );
+  } catch (err) {
+    handleError(err, ctx, "status");
+  }
+});
+
+// --- get status of current queue
+const queue = Composer.command("queue", async (ctx) => {
+  try {
+    return await ctx.telegram.sendMessage(
+      ctx.chat.id,
+      `TODO: Send queue info to user`
+    );
+  } catch (err) {
+    handleError(err, ctx, "status");
+  }
+});
+
+export default Composer.compose([start, help, status, queue]);
